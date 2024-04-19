f4ea1949b22c4505f2dc07af7435801334e37fa6
Viuviu42 <lengyelcsongi@gmail.com>
2024-04-19 13:50:14 +0200

    5. feladat

--- Kimi.py
+++ Kimi.py
@@ -1,13 +1,17 @@
 class Kimi:
-    def __init__(self,date,bigp,placement,pont,kon,finish,circl,error) -> None:
+    def __init__(self,date,bigp,placement,end,pont,kon,finish,circl,error) -> None:
         self.date = date
         self.bigp = bigp
         self.placement = placement
+        self.end = end
         self.pont = pont
         self.kon = kon
         self.finish = finish
         self.circl = circl
         self.error = error
+
+    def __str__(self) -> str:
+        return self.finish
         
 
 
@@ -17,12 +21,13 @@ class Kimi:
 file = open("kimi.csv", "tr", encoding="utf-8")
 kimi = []
 count = 0
+stat = {}
 
 file.readline()
 
 for row in file:
     row = row.strip().split(";")
-    kimi.append(Kimi(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]))
+    kimi.append(Kimi(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8]))
     count += 1
 
 if count == len(kimi):
@@ -32,4 +37,16 @@ print("4. feladat: Magyar Nagydíj helyezései")
 for i in kimi:
     if i.bigp == "Magyar Nagydíj":
         if i.placement != "":
-            print(f"{i.date} : {i.placement} hely")
+            print(f"\t {i.date} : {i.placement}. hely")
+
+for i in kimi:
+    if i.finish == "N":
+        if i.error in stat.keys():
+            stat[i.error] += 1
+        else:
+            stat[i.error] = 1
+ 
+print("5. feladat: Hibastatisztika")
+for k,t in stat.items():
+    if t > 1:
+        print("\t", k, ":", t)
